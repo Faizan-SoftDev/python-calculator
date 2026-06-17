c7a6a5209a78271e8ed239285466273fb355cd32
Muhammad Faizan <muhammadfaizan.dev48@gmail.com>
2026-06-17 16:17:36 +0500

    Add calculator.py and main.py

---
+++ calculator.py
@@ -0,0 +1,8 @@
+def add(num_1, num_2):
+    return num_1 + num_2
+def subtract(num_1, num_2):
+    return num_1 - num_2
+def multiply(num_1, num_2):
+    return num_1 * num_2
+def devide(num_1, num_2):
+    return num_1 / num_2
---
+++ main.py
@@ -0,0 +1,16 @@
+import calculator
+num_1 = 16
+num_2 = 7
+total =calculator.add(num_1,num_2  )
+diff = calculator.subtract(num_1,num_2 )
+multiply = calculator.multiply(num_1,num_2 )
+division = calculator.devide(num_1,num_2 )
+
+quot = calculator.devide(num_1, num_2)
+print(num_1, '+', num_2, '=', total)
+print(num_1, '-', num_2, '=', diff)
+print(num_1, '*', num_2, '=', multiply)
+print(num_1, '/', num_2, '=', division)
+
+
+
